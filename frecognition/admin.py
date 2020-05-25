19b25c832a2b8f071cc363ff1184ff9f285d3e54
Flrotm <francesco.uccell@utec.edu.pe>
2020-05-25 15:49:36 -0500

    asistencia

--- frecognition/admin.py
+++ frecognition/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Image,Clases,Alumno,Profesor,Admin,Embedding
+from .models import Image,Clases,Alumno,Profesor,Admin,Embedding,Asistencia
 
 # Register your models here.
 admin.site.register(Image)
 admin.site.register(Clases)
 admin.site.register(Alumno)
 admin.site.register(Profesor)
+admin.site.register(Asistencia)
 admin.site.register(Admin)
 admin.site.register(Embedding)
