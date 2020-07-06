02e50afe62705fa6fbbea21f23a4292aaa78acd0
StephanoWurttele <stephano.wurttele@utec.edu.pe>
2020-07-06 17:28:10 -0500

    Toma de asistencia terminada

--- frecognition/api/views.py
+++ frecognition/api/views.py
@@ -105,27 +105,41 @@ class rollcall(APIView):
 		return JsonResponse({"alumnos" : asistentes})
 
 	def post(self, request):
-		clase = Clases.objects.filter(codigo=request.data['course'])[0]
-		today = date.today()
-		def inline_knn(alumno_vec):
-			for candidato in Embedding.objects.all():
-				candidato_vec = candidato.atributos
+		option = request.data['option'] == 0
+		if option:
+			clase = Clases.objects.filter(codigo=request.data['course'])[0]
+			today = date.today()
+			def inline_knn(alumno_vec):
+				for candidato in Embedding.objects.all():
+					candidato_vec = candidato.atributos
 
-				dist = 0
-				for i in range(len(alumno_vec)):
-					dist += (float(candidato_vec[i]) - alumno_vec[i]) ** 2
-				dist = math.sqrt(dist)
+					dist = 0
+					for i in range(len(alumno_vec)):
+						dist += (float(candidato_vec[i]) - alumno_vec[i]) ** 2
+					dist = math.sqrt(dist)
 
-				epsilon = 0.5
-				if dist < epsilon:
-					return candidato.codigo_alumno
-		
-		alumnos = []
-		embeddings = request.data['candidatos']
-		for embedding in embeddings:
-			alumno = inline_knn(embedding)
-			if (alumno is not None and len(alumno.clases.filter(codigo=clase.codigo)) == 1):
-				asistencia = Asistencia(codigo = str(clase.codigo)+str(today)+str(alumno.codigo), codigo_alumno = alumno, codigo_clase = clase, fecha = today)
-				asistencia.save()
-				alumnos.append(alumno.nombre)
-		return JsonResponse({"alumnos" : alumnos})
+					epsilon = 0.5
+					if dist < epsilon:
+						return candidato.codigo_alumno
+			alumnos = []
+			embeddings = request.data['candidatos']
+			for embedding in embeddings:
+				alumno = inline_knn(embedding)
+				if (alumno is not None and len(alumno.clases.filter(codigo=clase.codigo)) == 1):
+					asistencia = Asistencia(codigo = str(clase.codigo)+str(today)+str(alumno.codigo), codigo_alumno = alumno, codigo_clase = clase, fecha = today)
+					asistencia.save()
+					alumnos.append(alumno.nombre)
+			return JsonResponse({"alumnos" : alumnos})
+		else:
+			clase = Clases.objects.filter(codigo=request.data['course'])[0]
+			today = date.today()
+			asistentes = []
+			alumnos = request.data['alumnos']
+			asistencia_en_clase = Asistencia.objects.filter(fecha = today).filter(codigo_clase = clase)
+			for id in alumnos:
+				# alumno = Alumno.objects.filter(codigo = id)
+				if (asistencia_en_clase.filter(codigo_alumno = id)):
+					asistentes.append(1)
+				else:
+					asistentes.append(0)
+			return JsonResponse({"alumnos" : asistentes})
